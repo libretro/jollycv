2790d59c7194d29e83f8603ffd7e9da17f11575e
Rupert Carmichael <5050061-carmiker@users.noreply.gitlab.com>
2026-09-11 00:30:20 -0400

    libretro: Update version

--- libretro/libretro.c
+++ libretro/libretro.c
@@ -573,7 +573,7 @@ void retro_set_controller_port_device(unsigned port, unsigned device) {
 void retro_get_system_info(struct retro_system_info *info) {
     memset(info, 0, sizeof(*info));
     info->library_name     = "JollyCV";
-    info->library_version  = "2.0.0";
+    info->library_version  = "2.1.0";
     info->need_fullpath    = false;
     info->valid_extensions = "col|rom|myv|bin";
 }
